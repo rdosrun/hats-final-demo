c0c140a849f22ebe1726337117713374bbf9ab52
richardh <rdosrun@gmail.com>
2024-12-11 16:48:47 +0000

    mobile fixes

--- public/script/page_route.js
+++ public/script/page_route.js
@@ -14,7 +14,11 @@ function loadPage(route) {
             if (route == "/views/products") {
                 loadScriptDynamically("/product.js");
             }
-        })
+        });
+    const navLinks = document.querySelector('.nav-links');
+  if (navLinks) {
+    navLinks.style.display = 'none'; // Explicitly hide the nav-links
+  }
 }
 
 function loadScriptDynamically(src) {
