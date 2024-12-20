e234105e20b908f6f97aafcb00eca9fe8ef26f17
richardh <rdosrun@gmail.com>
2024-12-20 22:06:29 +0000

    sign-in work

--- public/script/script.js
+++ public/script/script.js
@@ -2,7 +2,7 @@
     });
   const msalConfig = {
     auth: {
-      clientId: 'a0df89da-e117-48bd-84d3-376b3b0df411', // Replace with your application ID
+      clientId: '95e880e8-e54d-4d01-a26c-052cff7e9592', // Replace with your application ID
       authority: 'https://login.microsoftonline.com/6ba30675-1170-422f-89c3-0a43af4a4534', // Replace with your tenant ID
       redirectUri: window.location.origin
     }
