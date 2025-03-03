7b6cb0c711f5b73c612a87541549fb1183f673a0
rdosrun <rdosrun@gmail.com>
2025-03-03 10:59:17 -0700

    chore: update test.txt with new content

--- authConfig.js
+++ authConfig.js
@@ -2,9 +2,10 @@ const REDIRECT_URI = process.env.REDIRECT_URI || 'http://localhost:3000/auth/mic
 const POST_LOGOUT_REDIRECT_URI = process.env.POST_LOGOUT_REDIRECT_URI || 'http://localhost:3000/';
 
 module.exports = {
+    
     auth: {
-        clientId: process.env.MICROSOFT_CLIENT_ID,
-        authority: `https://login.microsoftonline.com/${process.env.TENANT_ID}`,
+        clientId: '95e880e8-e54d-4d01-a26c-052cff7e9592',
+        authority: 'https://login.microsoftonline.com/6ba30675-1170-422f-89c3-0a43af4a4534',
         clientSecret: process.env.MICROSOFT_CLIENT_SECRET,
     },
     system: {
